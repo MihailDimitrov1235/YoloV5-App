0e4ef11e1d0a151ac51e19069e172397f8e6df56
Misho <Mighty.strong1235@gmail.com>
2022-12-18 17:08:18 +0200

    checks if files exist in images

--- checkFolder.py
+++ checkFolder.py
@@ -1,12 +1,19 @@
 import os
 import time
+from detect import run
 
 folder_path = './data/images'
 
 while True:
   if not os.listdir(folder_path):
-    print('Folder is empty')
+    #empty
+    print("empty")
   else:
-    print('Folder is not empty')
+    run() #not empty
+
+    # deletes all files in the folder
+
+    # for file in os.listdir(folder_path):
+    #   os.remove(os.path.join(folder_path, file))
 
   time.sleep(1)  # check every 1 second
